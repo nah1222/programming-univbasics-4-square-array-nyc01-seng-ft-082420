df4b9169baf194285872c72a22f4ad3ad497d15b
practical-kernel-3029 <nah1222@yahoo.com>
2020-08-03 17:51:58 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,10 @@
-def square_array(numbers)
-  result = Array.new(numbers.size)
+def square_array(array)
+  result = Array.new(array.size)
 
   i = 0
 
-  while i < numbers.size
-    result[i] = numbers[i] ** 2
+  while i < array.size
+    result[i] = arraylearn[i] ** 2
     i += 1
   end
 
